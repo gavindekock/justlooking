ad111f333752feee005104699143ea861d1da00c
Gavin de Kock <gavindekock@gmail.com>
2017-10-28 23:40:45 +0100

    Initial commit

---
+++ justlooking.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace justlooking.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
